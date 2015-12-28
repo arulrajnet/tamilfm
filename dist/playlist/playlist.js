2cfcf782421afe8ace31c91d1f559b0153d33a96
Arul <me@arulraj.net>
2015-12-28 23:22:38 +0530

    GH pages initail commit

---
+++ dist/playlist/playlist.js
@@ -0,0 +1,127 @@
+var jsonPlaylist = [
+  {
+    title: "ARR Lite",
+    mp3: "http://streaming.radionomy.com/1234554321"
+  },
+  {
+    title: "Raja Sir Lite",
+    mp3: "http://streaming.radionomy.com/IRLITE-stream"
+  },
+  {
+    title: "Raja Sir",
+    mp3: "http://streaming.radionomy.com/IR2IR2IRrdo"
+  },
+  {
+    title: "Illayarajavin Thevitatha Padalgal!",
+    mp3: "http://listen.radionomy.com/IllayarajavinThevitathaPadalgal-"
+  },
+  {
+    title: "Harris",
+    mp3: "http://streaming.radionomy.com/hjr-radio"
+  },
+  {
+    title: "SPB",
+    mp3: "http://streaming.radionomy.com/SPstreamlink"
+  },
+  {
+    title: "Puthu Paadal",
+    mp3: "http://streaming.radionomy.com/PPR-radio"
+  },
+  {
+    title: "YSR",
+    mp3: "http://streaming.radionomy.com/arrstream"
+  },
+  {
+    title: "Tamil lite",
+    mp3: "http://streaming.radionomy.com/Tlr-radio"
+  },
+  {
+    title: "A.R.Rahman Radio",
+    mp3: "http://streaming.radionomy.com/ARRahman-Radio"
+  },
+  {
+    title: "Yuvan Shankar Raja Radio",
+    mp3: "http://streaming.radionomy.com/Yuvan-Shankar-Raja-Radio"
+  },
+  {
+    title: "Mukil App",
+    mp3: "http://37.130.224.90:80/live"
+  },
+  {
+    title: "Radio City Tamil",
+    mp3: "http://216.245.201.73:9948/;?icy=http"
+  },
+  {
+    title: "Lankasri FM",
+    mp3: "http://159.253.131.114:80"
+  },
+  {
+    title: "Varnam FM",
+    mp3: "http://198.178.123.8:8402"
+  },
+  {
+    title: "Chellam FM",
+    mp3: "http://5.63.145.172:7124"
+  },
+  {
+    title: "Tamil Kuyil FM",
+    mp3: "http://173.193.219.163:8095"
+  },
+  {
+    title: "Vanavil FM",
+    mp3: "http://46.28.53.118:9000/stream"
+  },
+  {
+    title: "Tamilaruvi FM Live From Norway",
+    mp3: "http://66.90.103.189:8852"
+  },
+  {
+    title: "IBC Tamil",
+    mp3: "http://38.96.148.226:6176"
+  },
+  {
+    title: "Paris Tamil FM",
+    mp3: "http://198.178.123.17:7108"
+  },
+  {
+    title: "Hungama-Tamil-Hits",
+    mp3: "http://123.176.41.8:8632"
+  },
+  {
+    title: "Hungama-Tamil-classics",
+    mp3: "http://123.176.41.8:8264"
+  },
+  {
+    title: "Thendral World Radio",
+    mp3: "http://37.187.79.93:9116/stream"
+  },
+  {
+    title: "Geetham Tamil Radio",
+    mp3: "http://www.geethamradio.com:8020/lofi.mp3"
+  },
+  {
+    title: "Jil Jil Radio",
+    mp3: "http://live64.jiljilradio.com/;stream1.mp3"
+  },
+  {
+    title: "Tamil Beat Radio",
+    mp3: "http://streaming.radionomy.com/TBstream"
+  },
+  {
+    title: "Raja and Yuvan",
+    mp3: "http://streaming.radionomy.com/IRYUVIstream"
+  },
+  {
+    title: "Raja and ARR",
+    mp3: "http://streaming.radionomy.com/ILMstream"
+  },
+  {
+    title: "Puradsi FM",
+    mp3: "http://puradsifm.net:9994/;"
+  },
+  {
+    title: "Tambura Radio Tamil Comedy",
+    mp3: "http://listen.radionomy.com/TamburaRadioTamilComedy"
+  }
+];
+
