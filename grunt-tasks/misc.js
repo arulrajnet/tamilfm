ae6f965e4eb96b43aa1775a505820451d5eff108
Arul <me@arulraj.net>
2016-01-24 00:31:55 +0530

    CNAME added

--- grunt-tasks/misc.js
+++ grunt-tasks/misc.js
@@ -10,7 +10,7 @@ module.exports = {
 			dev: {
 				files: [
 					{expand: true, cwd: '<%= folders.src %>/js/app/playlist',  src: ['*'], dest: '<%= folders.build %>/playlist'},
-					{expand: true, cwd: '<%= folders.src %>/',  src: ['*.html'], dest: '<%= folders.build %>/'}
+					{expand: true, cwd: '<%= folders.src %>/',  src: ['*.html', 'CNAME'], dest: '<%= folders.build %>/'}
 				]
 			}
 		},
