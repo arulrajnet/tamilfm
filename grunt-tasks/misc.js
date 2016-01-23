d75c0404d021ee2dc102306f8141d149b8ebcdea
Arul <me@arulraj.net>
2016-01-24 00:04:48 +0530

    Deploy script to deploy in gh-pages - added script from https://github.com/X1011/git-directory-deploy/blob/master/deploy.sh - added fabfile.py for operational commands

--- grunt-tasks/misc.js
+++ grunt-tasks/misc.js
@@ -5,12 +5,12 @@ module.exports = {
 			build: {
 				files: [
 					{expand: true, cwd: '<%= folders.src %>/img',  src: ['*'], dest: '<%= folders.build %>/img'}
-					
 				]	
 			},
 			dev: {
 				files: [
-					{expand: true, cwd: '<%= folders.src %>/js/app/playlist',  src: ['*'], dest: '<%= folders.build %>/playlist'}
+					{expand: true, cwd: '<%= folders.src %>/js/app/playlist',  src: ['*'], dest: '<%= folders.build %>/playlist'},
+					{expand: true, cwd: '<%= folders.src %>/',  src: ['*.html'], dest: '<%= folders.build %>/'}
 				]
 			}
 		},
